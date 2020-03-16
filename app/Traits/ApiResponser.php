d46c8601c5170b9e5ce9495428e9c5b0e49ca740
mohamedkhalil1 <mohamedkhalil125445@gmail.com>
2020-03-16 10:26:05 +0200

    add paginate and cache for API Response

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,13 +2,16 @@
 
 namespace App\Traits;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 trait ApiResponser{
     private function successResponse($data ,$code){
 
-        return response()->json(['data' => $data] , $code);
+        return response()->json($data , $code);
     }
 
     protected function errorResponse($message,$code){
@@ -20,15 +23,18 @@ trait ApiResponser{
                 return $this->successResponse(['data' => $collection],$code);
             }
         $transformer = $collection->first()->transformer;
+        $collection = $this->filterData($collection,$transformer);
         $collection = $this->sortData($collection,$transformer);
+        $collection= $this->paginate($collection);
         $collection = $this->transformData($collection,$transformer);
-        return $this->successResponse($collection, $code);
+        $collection = $this->cacheResponse($collection);
+        return $this->successResponse(['data' => $collection], $code);
     }
 
     protected function showOne(Model $instance , $code=200){
        $transformer = $instance->transformer;
        $instance = $this->transformData($instance,$transformer);
-       return $this->successResponse($instance,$code);
+       return $this->successResponse(['data' => $instance],$code);
     } 
 
     protected function getMessage($message,$code=200){
@@ -38,9 +44,19 @@ trait ApiResponser{
     protected function transformData($data,$transformer)
     {
         $transformation = fractal($data,new $transformer);
-        return $transformation->toArray();
+
+        return $transformation->toArray()['data'];
     }
 
+    private function filterData(Collection $collection , $transformer){
+        foreach(request()->query() as $query=>$value){
+            $attribute = $transformer::originAttribute($query);
+            if(isset($attribute,$value)){
+                $collection = $collection->where($attribute,$value);
+            }
+        }
+        return $collection;
+    }
     private function sortData(Collection $collection,$transformer)
     {
         if(request()->has('sort_by')){
@@ -48,6 +64,36 @@ trait ApiResponser{
             $collection = $collection->sortBy->{$attribute};
         }
         return $collection;
+    }
+
+    private function paginate(Collection $collection){
+
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+        request()->validate($rules);
+        $perPage =15;
+
+        if(request()->has('per_page')){
+            $perPage = (int) request()->per_page;
+        }
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        
+        $result = $collection->slice(($page-1)*$perPage,$perPage)->values();
+        $paginated= new  LengthAwarePaginator($result,$collection->count(),$perPage,$page,[
+            'path' => LengthAwarePaginator::resolveCurrentPath()
+        ]);
+            // for another attribute as sort_by , filter 
+        $paginated->appends(request()->all());    
+        return $paginated;
+    }
+
+    private function cacheResponse($data){
+        $url = request()->url();
+        
+        return Cache::remember($url, 30/60, function()use($data){
+            return $data;
+        });
     }
 }
 
